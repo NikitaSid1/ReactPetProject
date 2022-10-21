670cd64af83c685df834576334baa03dda487025
Nikita <sidmid165@gmail.com>
2022-10-21 22:39:07 +0300

    feat: refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 import './index.scss';
 
 import {
