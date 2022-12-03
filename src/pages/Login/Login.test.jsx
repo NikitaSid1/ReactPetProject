c8bfe78f6e5ae10f0ddeb4f8436b60911267675d
Nikita <sidmid165@gmail.com>
2022-12-03 18:37:38 +0200

    feat: login signup tests creation

---
+++ src/pages/Login/Login.test.jsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { renderWithRouter } from 'test';
+import { screen, waitFor } from '@testing-library/react';
+import mockAxios from 'jest-mock-axios';
+import userEvent from '@testing-library/user-event';
+
+import { enLanguage } from 'translations';
+import { Login } from './Login';
+
+const LOGIN_EMAIL = 'second@gmail.com';
+const LOGIN_PASSWORD = '123ds2r1dwasd';
+
+const render = (route = '/') => renderWithRouter(<Login />, { route });
+
+describe('Login', () => {
+  test('should render component', () => {
+    const component = render();
+
+    expect(component).toBeDefined();
+  });
+
+  test('should return response', async () => {
+    render();
+
+    await waitFor(() =>
+      userEvent.type(screen.getByLabelText(enLanguage.authorization_email), LOGIN_EMAIL),
+    );
+
+    await waitFor(() => expect(screen.getByDisplayValue(LOGIN_EMAIL)).toBeVisible());
+
+    await waitFor(() =>
+      userEvent.type(screen.getByLabelText(enLanguage.authorization_password), LOGIN_PASSWORD),
+    );
+
+    await waitFor(() => expect(screen.getByDisplayValue(LOGIN_PASSWORD)).toBeVisible());
+
+    userEvent.click(screen.getByRole('button', { name: enLanguage.authorization_login }));
+
+    await waitFor(() =>
+      expect(mockAxios).toHaveBeenCalledWith(
+        expect.objectContaining({
+          url: '/login',
+          data: { email: LOGIN_EMAIL, password: LOGIN_PASSWORD },
+        }),
+      ),
+    );
+  });
+});
---
+++ src/pages/Registration/Registration.test.jsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { renderWithRouter } from 'test';
+import { screen, waitFor } from '@testing-library/react';
+import mockAxios from 'jest-mock-axios';
+import userEvent from '@testing-library/user-event';
+
+import { enLanguage } from 'translations';
+import { Registration } from './Registration';
+
+const LOGIN_EMAIL = 'second@gmail.com';
+const LOGIN_PASSWORD = '123ds2r1dwasd';
+
+const render = (route = '/') => renderWithRouter(<Registration />, { route });
+
+describe('Registration', () => {
+  test('should render component', () => {
+    const component = render();
+
+    expect(component).toBeDefined();
+  });
+
+  test('should return response', async () => {
+    render();
+
+    await waitFor(() =>
+      userEvent.type(screen.getByLabelText(enLanguage.authorization_email), LOGIN_EMAIL),
+    );
+
+    await waitFor(() => expect(screen.getByDisplayValue(LOGIN_EMAIL)).toBeVisible());
+
+    await waitFor(() =>
+      userEvent.type(screen.getByLabelText(enLanguage.authorization_password), LOGIN_PASSWORD),
+    );
+
+    await waitFor(() => expect(screen.getByDisplayValue(LOGIN_PASSWORD)).toBeVisible());
+
+    userEvent.click(screen.getByRole('button', { name: enLanguage.authorization_signup }));
+
+    await waitFor(() =>
+      expect(mockAxios).toHaveBeenCalledWith(
+        expect.objectContaining({
+          url: '/signup',
+          data: { email: LOGIN_EMAIL, password: LOGIN_PASSWORD },
+        }),
+      ),
+    );
+  });
+});
